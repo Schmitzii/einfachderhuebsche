86bcc42792ba42bfb47972f91764eef7a2e99c34
Schmitzii <63397150+Schmitzii@users.noreply.github.com>
2020-06-02 18:17:56 +0200

    Add files via upload

---
+++ Discord TestBot/Commands/FunCommands.cs
@@ -0,0 +1,78 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_TestBot.Commands
+{
+    public class FunCommands : BaseCommandModule
+    {
+        [Command("Ping")]
+        [Description("Gibt Pong zurück")]
+        public async Task Ping(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Pong").ConfigureAwait(false);
+        }
+
+
+        [Command("Addieren")]
+        [Description("Addiert zwei Zahlen")]
+        public async Task Add(
+            CommandContext ctx,
+            [Description("1. Zahl")] int numberOne,
+            [Description("2. Zahl")] int numberTwo)
+        {
+            await ctx.Channel.SendMessageAsync("Der Rechenboss hat als Ergebnis " + (numberOne + numberTwo).ToString()).ConfigureAwait(false);
+        }
+
+        [Command("Subtrahieren")]
+        [Description("Subtrahiert zwei Zahlen")]
+        public async Task Subtract(
+            CommandContext ctx,
+            [Description("1. Zahl")] int numberOne,
+            [Description("2. Zahl")] int numberTwo)
+        {
+            await ctx.Channel.SendMessageAsync("Der Rechenboss hat als Ergebnis " + (numberOne - numberTwo).ToString()).ConfigureAwait(false);
+        }
+
+        [Command("multiplizieren")]
+        [Description("Multipliziert zwei Zahlen")]
+        public async Task Multiply(
+            CommandContext ctx,
+            [Description("1. Zahl")] int numberOne,
+            [Description("2. Zahl")] int numberTwo)
+        {
+            await ctx.Channel.SendMessageAsync("Der Rechenboss hat als Ergebnis " + (numberOne * numberTwo).ToString()).ConfigureAwait(false);
+        }
+
+        [Command("Dividieren")]
+        [Description("Dividiert zwei Zahlen")]
+        public async Task Divide(
+            CommandContext ctx,
+            [Description("1. Zahl")] int numberOne,
+            [Description("2. Zahl")] int numberTwo)
+        {
+            await ctx.Channel.SendMessageAsync("Der Rechenboss hat als Ergebnis " + (numberOne / numberTwo).ToString()).ConfigureAwait(false);
+        }
+
+
+        [Command("länge")]
+        [Description("Finde deine wahre Länge heraus")]
+        public async Task Laenge(CommandContext ctx)
+        {
+            Random random = new Random();
+            string[] länge = new string[7];
+            länge[0] = "=";
+            länge[1] = "==";
+            länge[2] = "===";
+            länge[3] = "====";
+            länge[4] = "=====";
+            länge[5] = "======";
+            länge[6] = "=======";
+            await ctx.Channel.SendMessageAsync(ctx.Member.Username + "'s Schlong ist so lang: 8" + länge[random.Next(7)] + "D");
+        }
+
+    }
+}
---
+++ Discord TestBot/Commands/TeamCommands.cs
@@ -0,0 +1,17 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_TestBot.Commands
+{
+    public class TeamCommands : BaseCommandModule
+    {
+        
+    }
+}
